c1fccdf267f8d0c4a7c580bfc93f52e2699db0a3
Christian Liljenquist <christianliljenquist@Christians-MacBook-Pro.local>
2018-08-22 13:42:41 -0400

    test

---
+++ WEEK 1/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+createCanvas(800, 400);
+background(204);
+}
+
+function draw() {
+  // put drawing code here
+  ellipse(400, 200, 300, 140); // the body
+  ellipse(400, 100, 50, 60); // the head
+  ellipse(390, 100, 10, 10);
+  ellipse(410, 100, 10, 10);
+}
