2c2621fc07154d102c33ec8e42fbd80b3201fdfa
Christian Liljenquist <christianliljenquist@Christians-MacBook-Pro.local>
2018-08-27 12:22:58 -0400

    change

--- WEEK 1/sketch.js
+++ WEEK 1/sketch.js
@@ -6,7 +6,8 @@ background(204);
 function draw() {
   // put drawing code here
   ellipse(400, 200, 300, 140); // the body
-  ellipse(400, 100, 50, 60); // the head
-  ellipse(390, 100, 10, 10);
+  ellipse(400, 200, 100, 100);
+  ellipse(3000, 100, 10, 10);
   ellipse(410, 100, 10, 10);
+
 }
