37693ccc4bd1da175952cb07c1cf2df0b2abc40e
christianliljenquist <35701501+christianliljenquist@users.noreply.github.com>
2018-09-03 14:16:59 -0400

    change

--- WEEK 1/sketch.js
+++ WEEK 1/sketch.js
@@ -1,27 +1,28 @@
 function setup() {
-createCanvas(1000, 1000);
+createCanvas(800, 800);
 background(204);
 }
 
 function draw() {
   // put drawing code here
-  fill( 64, 255, 0);
-  ellipse(100, 60, 80, 40); // the body
-fill( 255, 0, 0);
-  ellipse(80, 40, 10, 10);
-  ellipse(120, 40, 10, 10);
-  fill(0, 0, 255);
-  ellipse(100, 65, 40, 10);
+  fill(0)
+  beginShape();
+  vertex(50,120)
+  vertex(50,190);
+  vertex(20,70);
+  vertex(80,20);
+  vertex(210,60);
+  vertex(160,80);
+  vertex(200,90);
+  vertex(140,100);
+  vertex(130,120);
+  vertex(200,200);
+  vertex(70,150);
+  vertex(100,300);
 
-
-    fill(255,255,0);
-    triangle(80,80,40,120,120,120);
-    line(40,80,120,80);
-
-  ellipse(550,450,100,100);
-  rect(500,500,100,300);
-  quad(300,300,500,500,500,800,300,950);
-  quad(600,500,800,300,800,950,600,800);
+  endShape();
+  fill(255,0,0);
+  ellipse(155,60,8,8);
 
 
 }
