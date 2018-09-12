c7806217647e007c233a884010bb8b65147bdc53
Christian Liljenquist <cbl52@miami.edu>
2018-09-12 19:54:06 -0400

    change

--- WEEK 1/sketch.js
+++ WEEK 1/sketch.js
@@ -1,20 +1,21 @@
-var x = 50
-var y = 50
-var d = 150
-var s = 80
+var x = 20
+var y = 20
+var d = 100
+var s = 300
 
 function setup() {
-createCanvas(800, 800);
-background(204);
+createCanvas(480, 120);
+strokeWeight(2);
+
 }
 
 function draw() {
   // put drawing code here
+  background(204);
+  for(var i=20; i<400; i+=20){
+    line(i,0, i+i/2, 80)
+    fill(255,0,0);
+    ellipse(i+i/2, 80, 20,20);
 
-  beginShape();
-  rect(x,y,s, s);
-  rect(x+d,y, 2*s,2*s);
-  rect(x+2*d+s,y,3*s, 3*s);
-
-
+  }
 }
