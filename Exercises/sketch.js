1747f7db88f243cb2052c15a80ea488a7eba8824
Christian Liljenquist <cbl52@miami.edu>
2018-09-18 19:15:56 -0400

    change

--- Exercises/sketch.js
+++ Exercises/sketch.js
@@ -1,16 +1,25 @@
-var x= 240;
-var y=60;
-var d=10;
-
+var x=120
+var y=50
+var d=10
+var a=250
 
 function setup(){
-  createCanvas(500,500);
-  background(204);
+  createCanvas(480,120);
+background(204);
   noFill();
 }
 
-
 function draw(){
-  for(var i=1; i<15; i++);
-  ellipse(x,y,i*d,i*d);
+  ellipse(x,y,d,d);
+  ellipse(x,y,2*d,2*d);
+  ellipse(x,y,3*d,3*d);
+  ellipse(x,y,4*d,4*d);
+  ellipse(x,y,5*d,5*d);
+  ellipse(x,y,6*d,6*d);
+  ellipse(a,y,d,d,);
+  ellipse(a,y,2*d,2*d);
+  ellipse(a,y,3*d,3*d);
+  ellipse(a,y,4*d,4*d);
+  ellipse(a,y,5*d,5*d);
+  ellipse(a,y,6*d,6*d);
 }
