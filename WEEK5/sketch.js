3bea1c176e2585d2e041cbe8e8fe2515ba0866d9
Christian Liljenquist <cbl52@miami.edu>
2018-09-19 14:39:00 -0400

    change

---
+++ WEEK5/sketch.js
@@ -0,0 +1,21 @@
+function setup(){
+  createCanvas(480,120);
+  background();
+}
+
+function draw(){
+  if(){
+    fill(255,0,0);
+  } else {
+    fill(255);
+  }
+  rect(90,30,60,60);
+
+  if(){
+    fill(0,255,0);
+  } else{
+    fill(255);
+  }
+
+  rect(330,30,60,60);
+}
