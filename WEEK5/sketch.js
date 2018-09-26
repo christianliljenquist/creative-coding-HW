f3b3bfd99d8a77ee6d7b5b2a7b25fd73e07e070c
Christian Liljenquist <cbl52@miami.edu>
2018-09-26 12:19:18 -0400

    change

--- WEEK5/sketch.js
+++ WEEK5/sketch.js
@@ -1,31 +1,23 @@
-function setup(){
-  createCanvas(480,120);
-  background();
+function setup() {
+  createCanvas(480, 120);
+  background(204);
+  noStroke();
 }
 
-function draw(){
-  if(mouseIsPressed){
-    if(mouseX>90 && mouseX<150 && mouseY>30 && mouseY<90){
-      fill(255,0,0);
-  }else{
-  fill(255);
-  }
+function draw() {
 
-  }
-  rect(90,30,60,60);
+  if(mouseIsPressed && mouseButton== LEFT && mouseX>90 && mouseX<150 && mouseY>30 && mouseY<90 ) {
+    fill(255, 0, 0);
+  } else {
+    fill(255);
+  }
+rect(90, 30, 60, 60);
 
-  if(mouseX>330 && mouseX<390 && mouseY>30 && mouseY<90){
-    fill(0,255,0);
-  } else{
-    fill(255);
-  }
-
-  rect(330,30,60,60);
-
-}else{
-  fill(255);
-  rect(90,30,60,60);
-  rect(330,30,60,60);
-
+if(mouseIsPressed && mouseButton== LEFT && mouseY>30 && mouseY<90 && mouseX>330 && mouseX<390) {
+  fill(0, 255, 0);
+} else {
+  fill(255);
 }
+rect(330, 30, 60, 60);
+
 }
