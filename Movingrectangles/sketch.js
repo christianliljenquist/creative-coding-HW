8db9ac6137b4e07dff1a76a5672aa80b7f04d5e8
Christian Liljenquist <cbl52@miami.edu>
2018-09-26 09:37:27 -0400

    chnge

--- Movingrectangles/sketch.js
+++ Movingrectangles/sketch.js
@@ -1,14 +1,30 @@
-var x=20
-var y=40
+var x=240
+
 
 
 
 function setup(){
-  createCanvas(1000,1000);
+  createCanvas(500,500);
 
   background(204);
 }
 
 function draw(){
-  rect(x,x,y,y);
+
+if (keyIsPressed){
+  if(keyCode==LEFT_ARROW){
+    x=x-5;
+
+  }
+  if (keyCode==RIGHT_ARROW){
+    x=x+5;
+  }
+}
+
+  fill(255,0,0);
+  //when the key is pressed the 3rd value has to be changed
+  rect(0,0,x,height);
+  fill(0,0,255);
+  //when the key is pressed, the 1st and 3rd value have to be changed
+  rect(x,0,width-x,height);
 }
--- WEEK5/sketch.js
+++ WEEK5/sketch.js
@@ -4,14 +4,14 @@ function setup(){
 }
 
 function draw(){
-  if(mouseYIsPressed){
+  if(mouseIsPressed && mouseButton== LEFT && mouseX>90 && mouseX<150 && mouseY>30 && mouseY<100){
     fill(255,0,0);
   } else {
     fill(255);
   }
   rect(90,30,60,60);
 
-  if(mouseXIsPressed){
+  if(mouseXIsPressed && mouseButton== RIGHT && mouseY>30 && mouseY<90 && mouseX>330 && mouseX<390){
     fill(0,255,0);
   } else{
     fill(255);
