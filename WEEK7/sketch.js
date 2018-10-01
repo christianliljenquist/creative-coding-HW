204e4318ff24322ebe0b34ff9e8dd0c37da50458
Christian Liljenquist <cbl52@miami.edu>
2018-10-01 12:37:25 -0400

    change

--- WEEK7/sketch.js
+++ WEEK7/sketch.js
@@ -1,37 +1,14 @@
-var x = 25;
-var h = 20;
-var y = 25;
+var img;
 
-function setup(){
-  createCanvas(1200,400);
+function preload(){
+  img=loadImage('giraffe.jpg');
 }
 
-function draw (){
+function setup(){
+  createCanvas(480,240);
   background(204);
-  x=20;
-  rect(x,y,100,h);
-  x=x+100;
-  rect(x,y+h,100,h);
-  x=x+100;
-  rect(x,y+h*2,100,h);
-  x=x+100;
-  rect(x,y+h*3,100,h);
-  x=x+100;
-  rect(x,y+h*4,100,h);
-  x=x+100;
-  rect(x,y+h*5,100,h);
-  x=x+100;
-  rect(x,y+h*6,100,h);
-  x=x+100;
-  rect(x,y+h*7,100,h);
-  x=x+100;
-  rect(x,y+h*8.100,h);
-  x=x+100;
-  rect(x,y+h*9,100,h);
-  x=x+100;
-  rect(x,y+h*10,100,h);
-  x=x+100;
-  rect(x,y+h*11,100,h);
-  x=x+100;
-  rect(x,y+h*12,100,h);
+}
+
+function draw(){
+  Image(img,0,0);
 }
