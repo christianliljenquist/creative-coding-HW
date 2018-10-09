087ab6dfc11863c317395394ed5ad0046a6eaa83
Christian Liljenquist <cbl52@miami.edu>
2018-10-09 17:48:27 -0400

    change

--- WEEK5/sketch.js
+++ WEEK5/sketch.js
@@ -1,18 +1,21 @@
 var x=60;
+var y=40;
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(720, 400);
  
 }
 
 function draw() {
-background(204);
+background(0);
+fill(0,255,0);
+ellipse(500, 50, 50, 50);
 fill(255,255,0);
-arc(x,60,radius,radius,0.52,5.76);
+arc(x,60,90,90,0.52,5.76);
 if(keyIsPressed){
   if(keyCode ==LEFT_ARROW){
     x=x-5;
   }else if(keyCode==RIGHT_ARROW){
     x=x+5;
+      }
+    }
   }
-}
-}
