ce5276b988e67a71b19b3657bd1dab2a245bcdbd
Christian Liljenquist <cbl52@miami.edu>
2018-10-15 14:34:05 -0400

    change

--- MIDTERM/sketch.js
+++ MIDTERM/sketch.js
@@ -71,6 +71,8 @@ if(keyCode==RIGHT_ARROW){
     arc(200,300,200,180,360,180);
     fill(102, 255, 102);
     text('WOOOHOOO',20,50,400);
+    fill(255,0,0);
+    text('YAY',50,200,20);
   }
   else {
     line(100, 275, 300, 275);
