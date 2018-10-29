14ac5c72daeceb6b7b78354c491d13f7af224545
Christian Liljenquist <cbl52@miami.edu>
2018-10-29 12:46:30 -0400

    change

---
+++ WEEK10/sketch.js
@@ -0,0 +1,13 @@
+function drawRubik(){
+}
+
+function setup(){
+createCanvas(500,500);
+backgorund(204);
+drawRubix;
+}
+
+function draw(){
+  fill(255,0,0);
+  rect(100,100,100,100);
+}
--- WEEK9/sketch.js
+++ WEEK9/sketch.js
@@ -1,25 +1,28 @@
-var x=80;
-var y=0;
+var x = 80;
+var y = 60;
 var speed = 5;
 
 
-
-
-function setup(){
-  createCanvas(500,500);
-
-
+function setup() {
+  createCanvas(500, 500);
 }
 
-function draw(){
-background(0);
-stroke(255);
-y +=speed;
-line(x,y,x,y+20);
-line(2*x,y-30,2*x,y+20-30);
-line(3*x,y+50,3*x,y+20+50);
-line(4*x,y-10,4*x,y+20-10);
-if(y>height){
-  y=0;
+function draw() {
+  background(0);
+  currentTime = millis ();
+  if (currentTime < 5000) {
+     y += speed;
+    if (y > height) {
+        y = 0;
+      }
+    stroke(255);
+      line(x, y, x, y+20);
+      line(2*x, y-30, 2*x, y+20-30);
+      line(3*x, y-30, 3*x, y+20+50);
+      line(4*x, y-30, 4*x, y+20-10);
+  } else {
+fill(255, 255, 0);
+noStroke()
+ellipse(width/2, y, 30, 30);
 }
 }
