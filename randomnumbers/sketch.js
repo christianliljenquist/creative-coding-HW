70b848d37fdc2eed5e0129e9dfa09f5cfdc81a52
Christian Liljenquist <cbl52@miami.edu>
2018-10-29 13:08:21 -0400

    change

--- randomnumbers/sketch.js
+++ randomnumbers/sketch.js
@@ -1,23 +1,10 @@
-function setup () {
-  createCanvas (480, 240);
-  background (204);
-  drawARubiks (10, 10);
-  drawARubiks (70, 100);
-  drawARubiks (250, 30);
-  drawARubiks (360, 80);
-}
-
-function drawARubiks (x, y) {
-  // red
-  fill (255, 0, 0);
-  rect (x, y, 50, 50);
-  // green
-  fill (0, 255, 0);
-  rect (x+50, y, 50, 50);
-  // blue
-  fill (0, 0, 255);
-  rect (x, y+50, 50, 50);
-  // purple
-  fill (255, 0, 255);
-  rect (x+50, y+50, 50, 50);
+function setup(){
+  createCanvas(480,120);
+  textSize(24);
+  textAllign(CENTER);
+  backgroun(204);
+  var face= rollDice();
+  text(face,160,60);
+  face=rollDice();
+  text(face, 320,60);
 }
