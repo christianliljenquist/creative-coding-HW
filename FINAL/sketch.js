6ef3d7286c54c0a78560ca7e2691a07b04a9ec1d
Christian Liljenquist <cbl52@miami.edu>
2018-12-03 11:43:18 -0500

    change

--- FINAL/sketch.js
+++ FINAL/sketch.js
@@ -1,28 +1,10 @@
-function setup() {
-  createCanvas(400, 400);
-  textSize(50);
-  textAlign(CENTER);
-  angleMode(DEGREES);
+var button;
+
+function setup(){
+  createCanvas(240,240);
+  background(205);
 }
 
-function draw() {
-  background(255,0,0);
-  stroke(255);
-  strokeWeight(8);
-  noFill();
+button=createButton('1st Question');
+button.position(200,200);
 }
-
-if(keyCode==RIGHT_ARROW){
-  background(200, 255, 0);
-  stroke(255);
-  strokeWeight(8);
-  noFill();
-  ellipse(150, 150, 40, 40);
-  ellipse(250, 150, 40, 40);
-  if (mouseIsPressed) {
-    arc(200,300,200,180,360,180);
-    fill(102, 255, 102);
-    text('WOOOHOOO',20,50,400);
-    fill(255,0,0);
-    text('YAY',50,200,20);
-  }
